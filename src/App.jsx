d3e0ea902240b0ebeec978b9a68c6100bdf2c53f
astarta16 <ngedeshuri2001@gmail.com>
2023-08-15 23:22:32 +0400

    create tailwind colors and make markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-
+import btnImage from './assets/icon-arrow.svg';
 import { useState } from 'react';
 import './App.css';
+import './index.css';
 
 function App() {
   const [day, setDay] = useState('');
@@ -14,6 +15,74 @@ function App() {
   const[emptyDay, setEmptyDay] = useState(false);
   const[emptyMonth, setEmptyMonth] = useState(false);
   const[emptyYear, setEmptyYear] = useState(false);
+
+
+
+
+  return (
+    <div >
+      <form>
+      <label>
+      <p className="">day</p>
+        <input
+          value={day}
+          onChange={e => setDay(e.target.value)}
+          type="number"
+          placeholder='DD'
+        />
+      </label>
+      <label>
+      <p className="">Month</p>
+        <input
+          value={month}
+          onChange={e => setMonth(e.target.value)}
+          type="number"
+          placeholder='MM'
+        />
+      </label>
+      <label>
+      <p className="">Year</p>
+        <input
+          value={year}
+          onChange={e => setYear(e.target.value)}
+          type="number"
+          placeholder='YYYY'
+        />
+      </label>
+    </form>
+ 
+
+
+    <div className="line w-20 h-[1.5px] m-2 bg-lightgrey"></div>
+<button className="bg-purple">
+  <img src={btnImage} alt="" />
+</button>
+<div className="resultContainer">
+          <h1 className="text-black">
+            <span className="text-red-800 text-4xl m-2">
+              {Number.isNaN(resultYear) ? "--" : resultYear}
+            </span>
+            years
+          </h1>
+          <h1 className="text-black">
+            <span className="text-purple text-4xl m-2">
+              {Number.isNaN(resultMonth) ? "--" : resultMonth}
+            </span>
+            months
+          </h1>
+          <h1 className="text-black text-2xl">
+            <span className="text-red-800 text-4xl m-2">
+              {Number.isNaN(resultDay) ? "--" : resultDay}
+            </span>
+            days
+          </h1>
+        </div>
+      </div>
+    
+    
+
+
+  )
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      purple: "var(--color-purple)",
+      lightred: "var(--color-lightred)",
+      white: "var(--color-white)",
+      offwhite: "var(--color-offwhite)",
+      lightgrey: "var(--color-lightgrey)",
+      smokeygrey: "var(--color-smokeygrey)",
+      offblack: "var(--color-offblack)",
+    },
     extend: {},
   },
   plugins: [],
