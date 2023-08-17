24bf2a9b99bf703e4194d72d821daee5c64f0073
astarta16 <ngedeshuri2001@gmail.com>
2023-08-17 22:02:20 +0400

    add react logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,53 @@ import btnImage from './assets/icon-arrow.svg';
 import { useState } from 'react';
 import './App.css';
 
-
 function App() {
-  const [day, setDay] = useState('');
-  const [month, setMonth] = useState('');
-  const [year, setYear] = useState('');
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
 
+  const [age, setAge] = useState({
+    years: "--",
+    months: "--",
+    days: "--",
+  });
 
+  const [error, setError] = useState(null);
+
+  const calculateAge = () => {
+    const inputDate = new Date(`${year}-${month}-${day}`);
+
+    if (!day || !month || !year) {
+      setError("All fields are required.");
+      return;
+    }
+
+    if (isNaN(inputDate)) {
+      setError("Invalid date.");
+      return;
+    }
+
+    const currentDate = new Date();
+    if (inputDate > currentDate) {
+      setError("Date cannot be in the future.");
+      return;
+    }
+
+    const yearDiff = currentDate.getFullYear() - inputDate.getFullYear();
+    const monthDiff = currentDate.getMonth() - inputDate.getMonth();
+    const dayDiff = currentDate.getDate() - inputDate.getDate();
+
+    
+
+    setAge({
+      years: yearDiff,
+      months: monthDiff,
+      days: dayDiff,
+    });
+
+    setError(null);
+  };
+  
 
   return (
     <div className="flex justify-center items-center h-screen">
@@ -47,30 +87,39 @@ function App() {
             </label>
           </form>
         </div>
-  
+
+        {error && (
+          <div className="text-red mt-3">
+            {error}
+          </div>
+        )}
+
         <div className="flex items-center mb-4">
           <div className="flex-grow">
             <hr className="border-smokeyGrey" />
           </div>
-          <button className="bg-purple hover:bg-black ml-4 rounded-lg">
+          <button
+            className="bg-purple hover:bg-black ml-4 rounded-lg"
+            onClick={calculateAge}
+          >
             <img src={btnImage} alt="" />
           </button>
         </div>
-  
+
         <div className="mt-4 flex justify-center">
           <div className="flex flex-col items-center">
             <div className="mt-4 flex items-center">
-              <span className="text-purple text-4xl font-bold leading-110">--</span>
+              <span className="text-purple text-4xl font-bold leading-110">{age.years}</span>
               <p className="ml-2 text-4xl font-bold leading-110">YEARS</p>
             </div>
-  
+
             <div className="mt-4 flex items-center">
-              <span className="text-purple text-4xl font-bold leading-110">--</span>
+              <span className="text-purple text-4xl font-bold leading-110">{age.months}</span>
               <p className="ml-2 text-4xl font-bold leading-110">MONTH</p>
             </div>
-  
+
             <div className="mt-4 flex items-center">
-              <span className="text-purple text-4xl font-bold leading-110">--</span>
+              <span className="text-purple text-4xl font-bold leading-110">{age.days}</span>
               <p className="ml-2 text-4xl font-bold leading-110">DAYS</p>
             </div>
           </div>
@@ -78,12 +127,6 @@ function App() {
       </div>
     </div>
   );
-  
-
-   
-  
 }
 
-export default App;
-
-
+export default App
